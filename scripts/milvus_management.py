2dbf131634878e29337bfe67dbc27d93198f8c38
Sofía Cerdá Muñoz <sofia.cerda@sngular.com>
2025-05-26 13:08:05 +0000

    added milvus driver

---
+++ scripts/milvus_management.py
@@ -0,0 +1,54 @@
+import json
+
+import typer
+
+from app.services.milvus import DEFAULT_COLLECTION, DEFAULT_DATABASE, MilvusHandler
+
+cli = typer.Typer()
+
+TEST_DATA = "tests/fixtures/milvus_test_data.json"
+
+
+@cli.callback()
+def main(ctx: typer.Context):
+    ctx.ensure_object(dict)
+    milvus = MilvusHandler()
+    ctx.obj["milvus"] = milvus
+
+
+@cli.command(name="load_data")
+def load_initial_milvus_data(
+    ctx: typer.Context,
+    data_file: str = typer.Option(TEST_DATA, help="Path to load data"),
+    collection_name: str = typer.Option(DEFAULT_COLLECTION, help="Milvus collection name"),
+    db_name: str = typer.Option(DEFAULT_DATABASE, help="Milvus database name"),
+):
+    milvus: MilvusHandler = ctx.obj.get("milvus")
+    if not milvus.exists_collection(collection_name):
+        with open(data_file, encoding="utf-8") as f:
+            raw_docs: list[dict] = json.load(f)
+        texts = [doc.pop("text") for doc in raw_docs]
+        milvus.create_vector_store_from_texts(
+            texts=texts, metadatas=raw_docs, collection_name=collection_name, db_name=db_name
+        )
+
+
+@cli.command(name="remove_collection")
+def remove_milvus_collection(
+    ctx: typer.Context,
+    collection_name: str = typer.Option(DEFAULT_COLLECTION, help="Milvus collection name"),
+):
+    milvus: MilvusHandler = ctx.obj.get("milvus")
+    milvus.remove_collection(collection_name)
+
+
+@cli.command(name="remove_database")
+def remove_milvus_database(
+    ctx: typer.Context, db_name: str = typer.Option(DEFAULT_DATABASE, help="Milvus database name")
+):
+    milvus: MilvusHandler = ctx.obj.get("milvus")
+    milvus.remove_database(db_name)
+
+
+if __name__ == "__main__":
+    cli()
---
+++ tests/test_milvus.py
@@ -0,0 +1,34 @@
+import pytest
+
+from app.services.milvus import MilvusHandler
+
+
+class TestMilvusHandler:
+    test_db = "test_db"
+    test_collection = "test_collection"
+
+    @pytest.fixture(scope="class")
+    def milvus_conexion(self):
+        milvus = MilvusHandler()
+        yield milvus
+        milvus.client.drop_collection(self.test_collection)
+        milvus.client.drop_database(self.test_db)
+
+    def test_manage_database_and_collections(self, milvus_conexion: MilvusHandler):
+        milvus_conexion._use_database(self.test_db)
+        assert (
+            self.test_db in milvus_conexion.client.list_databases()
+        ), f"Database '{self.test_db}' should be in the list of databases"
+
+        assert (
+            milvus_conexion.client.list_collections() == []
+        ), "Collection list should be empty at this moment"
+        milvus_conexion.client.create_collection(self.test_collection, dimension=512)
+        assert milvus_conexion.client.list_collections() == [
+            self.test_collection
+        ], f"Collection list should contain ONLY the created collection '{self.test_collection}'"
+
+        milvus_conexion.remove_database(self.test_db)
+        assert (
+            self.test_db not in milvus_conexion.client.list_databases()
+        ), f"Database '{self.test_db}' should NOT be in the list of databases after removing it"
