358532aef1decb113ffec10ac8c8989d658463ac
Sofía Cerdá Muñoz <sofia.cerda@sngular.com>
2025-10-14 12:12:14 +0000

    update agent

--- segittur_commons/app/agents/agent.py
+++ segittur_commons/app/agents/agent.py
@@ -3,7 +3,6 @@ from typing import List
 
 from langchain_core.language_models.chat_models import BaseChatModel
 from langchain_core.prompts import (
-    BasePromptTemplate,
     ChatPromptTemplate,
     MessagesPlaceholder,
     PromptTemplate,
@@ -31,39 +30,33 @@ class Agent(ABC):
         return [getattr(cls, attr) for attr in cls.__dict__ if attr.endswith("_var")]
 
     @classmethod
-    def human_template(cls) -> str:
-        if not (args := cls._get_prompt_vars()):
-            template = f"{{{cls.user_input_var}}}"
-        else:
-            vars: list[str] = (
-                [cls.user_input_var] + args if cls.user_input_var not in args else args
-            )
-            template = "\n\n".join(
-                [f"**{arg.replace('_', ' ').capitalize()}**\n```\n{{{arg}}}\n```" for arg in vars]
-            )
-        return template
+    def placeholder_template(cls) -> str:
+        excluded_vars = [cls.messages_var, cls.user_input_var]
+        args = cls._get_prompt_vars()
+        if not (filtered_args := [e for e in args if e not in excluded_vars]):
+            return ""
 
-    @property
-    def messages_placeholder(self) -> MessagesPlaceholder:
-        return MessagesPlaceholder(variable_name=self.messages_var)
+        template = "\n\n".join(
+            [
+                f"**{arg.replace('_', ' ').capitalize()}**\n```\n{{{arg}}}\n```"
+                for arg in filtered_args
+            ]
+        )
+        return f"\n\n {template}"
 
     def create_simple_template(self) -> PromptTemplate:
         return PromptTemplate.from_template(self.prompt)
 
-    def create_messages_template(
-        self,
-        system_prompt: str | None = None,
-        human_prompt: str | None = None,
-        extra_messages: list = [],
-    ) -> ChatPromptTemplate:
-        human_message = (
-            [] if human_prompt == "" else [("human", human_prompt or self.human_template())]
-        )
+    def create_messages_template(self, system_prompt: str | None = None) -> ChatPromptTemplate:
         return ChatPromptTemplate.from_messages(
-            [("system", system_prompt or self.prompt), *human_message, *extra_messages]
+            [
+                ("system", (system_prompt or self.prompt) + self.placeholder_template()),
+                MessagesPlaceholder(variable_name=self.messages_var, optional=True),
+                MessagesPlaceholder(variable_name=self.user_input_var, optional=True),
+            ]
         )
 
-    def _create_template(self) -> BasePromptTemplate:
+    def _create_template(self) -> ChatPromptTemplate:
         return self.create_messages_template()
 
     def _create_chain(self) -> RunnableSequence:
