a672c93eca0692b17d693767fc7fb03c1dc614d6
Sofía Cerdá Muñoz <sofia.cerda@sngular.com>
2026-02-12 15:10:08 +0000

    fix ruff

--- segittur_commons/app/infrastructure/ai/llm/fake_open_ai.py
+++ segittur_commons/app/infrastructure/ai/llm/fake_open_ai.py
@@ -3,7 +3,7 @@ from typing import Any
 
 from langchain_core.language_models import GenericFakeChatModel
 from langchain_core.language_models.base import LanguageModelInput
-from langchain_core.messages import AIMessage, BaseMessage
+from langchain_core.messages import AIMessage
 from langchain_core.runnables import RunnableConfig
 from langchain_openai import ChatOpenAI
 
